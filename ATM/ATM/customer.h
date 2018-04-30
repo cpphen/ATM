10a967fecabc92993f022134ce550a79bbf34103
henry <btnysci@yahoo.com>
2018-04-30 11:43:32 -0700

    setup

--- ATM/ATM/customer.h
+++ ATM/ATM/customer.h
@@ -1,1 +1,17 @@
 #pragma once
+
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+#include "stdafx.h"
+
+class Customer {
+	private: 
+		string accountID;
+		string name;
+	public:
+		Customer();
+		Customer(string, string);
+		void view();
+		Customer operator= (const Customer&);
+};
+#endif
