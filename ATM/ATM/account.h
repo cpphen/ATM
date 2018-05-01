f0f50569644bae485a2c4cb933313451e0e63a00
henry <btnysci@yahoo.com>
2018-05-01 10:46:30 -0700

    account

--- ATM/ATM/account.h
+++ ATM/ATM/account.h
@@ -1,1 +1,21 @@
 #pragma once
+
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+#include "customer.h";
+
+class Account {
+	private:
+		float balance;
+		Customer cust;
+
+	public:
+		Account();
+		Account(float, Customer);
+		void makeDeposit(float);
+		bool makeWithdrawal(float);
+		float getBalance();
+		void view();
+		void adjustBalance();
+};
+#endif
