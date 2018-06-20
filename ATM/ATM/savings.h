ed04886063f605047a2587c9d051c9c01f4e7a2c
henry <btnysci@yahoo.com>
2018-06-19 21:07:56 -0700

    savings class

--- ATM/ATM/savings.h
+++ ATM/ATM/savings.h
@@ -6,14 +6,14 @@
 
 class Savings : public Account
 {
-	protected:
-		float interestRate; //Monthly Interest Rate
-	public:
-		Savings();
-		Savings(Customer &, double, double);
-		virtual void makeDeposit(double);
-		virtual bool makeWithdrawal(double);
-		virtual void adjustBalance();
-		virtual void view();
+protected:
+	float interestRate; //Monthly Interest Rate
+public:
+	Savings();
+	Savings(Customer &, double, double);
+	virtual void makeDeposit(double);
+	virtual bool makeWithdrawal(double);
+	virtual void adjustBalance();
+	virtual void view();
 };
 #endif
