7e451e50c3017993c4ef92b588561c2d910840e4
henry <btnysci@yahoo.com>
2018-06-20 20:59:10 -0700

    account

--- ATM/ATM/account.cpp
+++ ATM/ATM/account.cpp
@@ -1,36 +1,50 @@
-#include "account.h";
+/*
+Henry Lee
+CSIT 840
+*/
 
-Account::Account()
+#include "account.h"
+#include <iostream>
+using namespace std;
+
+
+Account::Account()// : cust()
 {
+	balance = 0;
+	Customer();
 
 }
-
-Account::Account(float balance, Customer cust)
+Account::Account(Customer& c, float userStartBal)
+{
+	balance = userStartBal;
+	cust = c;  //This is the call to the overloaded assignment operator, just like in previoes assignment where we had Date3 and Date4 created and then had Date4 = date3 =date2, date3 is calling the overloaded operator function and date2 is the parameter. In this program, cust is the caller and c the parameter.
+			   //member variable = parameter
+}
+void Account::makeDeposit(double userDepAmnt)
 {
 
-}
+	balance += userDepAmnt;
 
-void Account::makeDeposit(float balance)
+}
+bool Account::makeWithdrawal(double userWithdraw)
 {
-
+	balance -= userWithdraw;
+	return true;
 }
-
-bool Account::makeWithdrawal(float balance)
-{
-
-}
-
 float Account::getBalance()
 {
-
+	return balance;
 }
-
 void Account::view()
 {
-
+	cust.view();
+	cout << "Balance: $" << " " << balance << endl; //the balance is output here since the Customer class does not have access to the balance member variable in this Account class. So after the account class calls the customer view() (cust.view()), it returns back here to display balance.
 }
-
-void Account::adjustBalance()
+void Account::calcSavingsBalance(double InterestTotal)
 {
-
+	balance += InterestTotal;
+}
+void Account::calcCheckingBalance(int num)
+{
+	balance = num;
 }
