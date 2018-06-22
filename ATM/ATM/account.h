adcf1280023b0f94e3bd7f1d2510c150d0525bf9
henry <btnysci@yahoo.com>
2018-06-21 21:24:18 -0700

    checks cpp

--- ATM/ATM/account.h
+++ ATM/ATM/account.h
@@ -16,6 +16,7 @@ public:
 	virtual bool makeWithdrawal(double);
 	float getBalance();
 	void view();
+	void calcSavingsBalance(double);
 	void calcCheckingBalance(int);
 	virtual void adjustBalance() = 0;
 };
