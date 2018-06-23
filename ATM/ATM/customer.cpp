0ce6be3dd14573a9ea58cdce51d9c6bca299b880
henry <btnysci@yahoo.com>
2018-06-22 18:29:30 -0700

    customer implementation

--- ATM/ATM/customer.cpp
+++ ATM/ATM/customer.cpp
@@ -1,2 +1,31 @@
 #include "stdafx.h"
 
+#include <iostream>
+#include<string>
+using namespace std;
+#include "customer.h"
+
+Customer::Customer()
+{
+	accountID = " ";
+	name = " ";
+
+}
+Customer::Customer(string userName, string userID)
+{
+	accountID = userID;
+	name = userName;
+}
+
+Customer Customer::operator = (const Customer& c)
+{
+	this->name = c.name; 
+	this->accountID = c.accountID; 
+
+	return *this;
+}
+void Customer::view()
+{
+	cout << "Name: " << name << endl;
+	cout << "Account #: " << accountID << endl; 
+}
